5ec9bf8dedd48eefe2ab2183873152b7f4b54528
Stefany García <faangaar@gmail.com>
2024-12-06 15:21:41 -0600

    data frame

--- main.py
+++ main.py
@@ -122,6 +122,9 @@ def main():
     
     # Mostrar todos los resultados
     pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
     print(datos[['tweet', 'label', 'predicted_label']])
     print("End of the main function")
 
