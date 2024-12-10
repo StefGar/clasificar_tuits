48a0b7016a3016c0f3d041671e22eb0707641071
Stefany García <faangaar@gmail.com>
2024-12-10 14:35:43 -0600

    -

--- entrenamiento.py
+++ entrenamiento.py
@@ -51,7 +51,7 @@ print(datos['label'].value_counts())
 
 # Balancear el dataset
 min_count = datos['label'].value_counts().min()
-balanced_data = datos.groupby('label').apply(lambda x: x.sample(min_count)).reset_index(drop=True)
+balanced_data = datos.groupby('label').apply(lambda x: x.sample(min_count, replace=True)).reset_index(drop=True)
 
 # Dividir datos en entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(balanced_data['tweet'], balanced_data['label'], test_size=0.2, random_state=42)
